6c70313941675375dcbdab9866bf38e570b0a1f6
jie-ui <aliceyangkk@gmail.com>
2025-11-25 10:56:00 -0500

    modify

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,11 +1,9 @@
-// src/api/http.js
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, 
-  withCredentials: false,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
-
 
 http.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
