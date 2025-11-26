c31555664a83c1033bac8ce70da4c52a21ba27ce
jie-ui <aliceyangkk@gmail.com>
2025-11-26 16:33:02 -0500

    fix: updated auth system & user schema

--- server/models/User.js
+++ server/models/User.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ["user", "admin"],
-    default: "User"
+    default: "user"
   },
   createdAt: {
     type: Date,
